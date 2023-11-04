8738a22ec78b0ac9d021a12b7be129b707426ccd
Surbhi Vandana <svandu08@gmail.com>
2023-11-04 17:00:16 +0530

    experiment of own library in vite

--- vitereact01/src/App.jsx
+++ vitereact01/src/App.jsx
@@ -1,9 +1,13 @@
 import Demo from "./demo"
 function App() {
 
+  const username = "chai aur code"
+
   return (
     <div>
       <h1>hello welcome to vite</h1>
+      <p>{username}</p>
+      {/* expression  */}
       <Demo />
     </div>
   )
--- vitereact01/src/main.jsx
+++ vitereact01/src/main.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Custom App! |</h1>
+//     </div>
+//   );
+// }
+
+// const reactElement = { // this object could not run because the react has its own rule that it can expect what type of element in the object and these props are our own prop
+//   type: 'a', //define the type of the element
+//   props: {
+//       href: "https://google.com",
+//       target: '_blank'
+//   },
+//   children: "Click me to visit google"
+// }
+
+// const anotherReactElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit google
+//   </a>
+// );
+
+// const anotherUser = "chai aur react"
+
+// const reactElement = React.createElement( //babel inject this in react
+//   "a",
+//   { href: "https://google.com", target: "_blank" },
+//   "click me to google",
+//   anotherUser //evaluated expression
+// ); // create element is the own property of react where we can create element and it take tag, attributes, text in its parameter
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <App />
+  // MyApp()
+  // anotherReactElement
+  // reactElement
+  // </React.StrictMode>,
+);
