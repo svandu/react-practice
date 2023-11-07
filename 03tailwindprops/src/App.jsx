20dc55a03a6f1dd96dfbfd66cb8598d3da65255f
Surbhi Vandana <svandu08@gmail.com>
2023-11-07 21:50:28 +0530

    some minor changes

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind test </h1>
 
-      <div className="relative h-[400px] w-[300px] rounded-md">
+      <div className="relative h-[400px] w-[300px] rounded-md p-4">
       <img
         src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
         alt="AirMax Pro"
